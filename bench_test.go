047d8ee07e29b295ebf5d9832fa6bdf4f3f8a769
guzenok <alexander.guzev@gmail.com>
2018-10-18 02:20:17 +1000

    sync and async benchmarks

--- bench_test.go
+++ bench_test.go
@@ -2,6 +2,7 @@ package memkvdb
 
 import (
 	"math/rand"
+	"sync"
 	"testing"
 	"time"
 )
@@ -12,19 +13,12 @@ const (
 	READ_RATE = 10
 )
 
-func BenchmarkDB(b *testing.B) {
+func BenchmarkDbSync(b *testing.B) {
+	val := make([]byte, VAL_LEN)
+	rand.Read(val[:])
 
 	key := make([]byte, KEY_LEN)
-	val := make([]byte, VAL_LEN)
-
-	_, err := rand.Read(key[:])
-	if err != nil {
-		b.Fatal(err)
-	}
-	_, err = rand.Read(val[:])
-	if err != nil {
-		b.Fatal(err)
-	}
+	rand.Read(key[:])
 
 	for name, store := range map[string]MemStore{
 		"Map":     CreateMapStore(),
@@ -52,6 +46,51 @@ func BenchmarkDB(b *testing.B) {
 					b.Fatal(err)
 				}
 			}
+		})
+	}
+}
+
+func BenchmarkDbAsync(b *testing.B) {
+	val := make([]byte, VAL_LEN)
+	rand.Read(val[:])
+
+	for name, store := range map[string]MemStore{
+		"Map":     CreateMapStore(),
+		"SyncMap": CreateSyncMapStore()} {
+		b.Run(name, func(b *testing.B) {
+
+			expiration := 30 * time.Second
+			db, err := New(expiration, store)
+			wg := &sync.WaitGroup{}
+
+			b.ResetTimer()
+			for i := 0; i < b.N/2/READ_RATE; i++ {
+				wg.Add(1)
+				go func(i int) {
+					defer wg.Done()
+
+					key := make([]byte, KEY_LEN)
+					rand.Read(key[:])
+
+					for j := 0; j < 2*READ_RATE; j++ {
+						key[i%KEY_LEN] = byte(rand.Int())
+						err = db.Set(key, val)
+						if err != nil {
+							b.Fatal(err)
+						}
+
+						if i%READ_RATE != 0 {
+							continue
+						}
+
+						_, err = db.Get(key)
+						if err != nil {
+							b.Fatal(err)
+						}
+					}
+				}(i)
+			}
+			wg.Wait()
 		})
 	}
 }
